52f565e1cc1920aa0804b3ac4615debc8b63542d
Punit <jpunit2005@gmail.com>
2025-02-28 09:24:05 +0530

    landing page change

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from './pages/Login'
 import { Routes,Route } from 'react-router-dom'
 // import ProtectedRoute from './pages/ProtectedRoute'
 import FitnessDashboard from './pages/FitnessDashboard'
+import LandingPage from './components/LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
     <>
 
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/dashboard' element={<FitnessDashboard/>}/>
